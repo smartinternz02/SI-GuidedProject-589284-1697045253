e906128aaef7b96417444999c60612f88e6f2aef
akshit <akshitbahl.ab@gmail.com>
2023-11-09 10:44:52 +0100

    added login/register functionality

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,12 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask.config import Config
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 import xgboost as xgb
 import numpy as np
 import pandas as pd 
@@ -6,28 +14,124 @@ import pickle
 import warnings
 warnings.filterwarnings("ignore")
 
-model=pickle.load(open('training\projectPGP.pkl','rb'))
+model=pickle.load(open(r'C:\Users\akshi\OneDrive\Desktop\Project PayGuard Plus\flask\training\projectPGP.pkl','rb'))
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY']='Success@2024'
+
+db=SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(20), nullable=False, unique=True)
+    password=db.Column(db.String(80), nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"{self.username}"
+    
+    
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+            
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
 
-@app.route('/about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('home.html')
+
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
+
+@app.route('/about')
 def about_page():
     return render_template('about.html')
 
-@app.route('/team.html')
+@app.route('/team')
 def team_page():
     return render_template('team.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact_page():
     return render_template('contact.html')
 
-@app.route('/services.html',methods=["GET","POST"])
+@app.route('/services',methods=["GET","POST"])
+@login_required
 def predict():
     if request.method=="GET":
         return render_template('services.html')
