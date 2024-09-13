26a19ee81dba5e9f09402c284c8866f056edca31
Akshit Bahl <akshitbahl.ab@gmail.com>
2024-09-13 15:30:41 +0530

    debug set to false, production initiated

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask,render_template,request,url_for,redirect
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    login_required,
+    logout_user,
+    current_user,
+)
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -8,73 +15,83 @@ from flask_bcrypt import Bcrypt
 
 import xgboost as xgb
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import pickle
 import warnings
+
 warnings.filterwarnings("ignore")
 
-model=pickle.load(open("training/projectPGP.pkl",'rb'))
+model = pickle.load(open("training/projectPGP.pkl", "rb"))
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SECRET_KEY']='Success@2024'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "Success@2024"
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-
 class User(db.Model, UserMixin):
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(20), nullable=False, unique=True)
-    password=db.Column(db.String(80), nullable=False)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
     def __repr__(self) -> str:
         return f"{self.username}"
-    
-    
+
+
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    username = StringField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "Username"},
+    )
 
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=8, max=20)],
+        render_kw={"placeholder": "Password"},
+    )
 
-    submit = SubmitField('Register')
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
+        existing_user_username = User.query.filter_by(username=username.data).first()
         if existing_user_username:
             raise ValidationError(
-                'That username already exists. Please choose a different one.')
-            
+                "That username already exists. Please choose a different one."
+            )
+
+
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    username = StringField(
+        validators=[InputRequired(), Length(min=4, max=20)],
+        render_kw={"placeholder": "Username"},
+    )
 
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    password = PasswordField(
+        validators=[InputRequired(), Length(min=8, max=20)],
+        render_kw={"placeholder": "Password"},
+    )
 
-    submit = SubmitField('Login')
+    submit = SubmitField("Login")
 
-@app.route('/')
+
+@app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -82,27 +99,24 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
+                return redirect(url_for("dashboard"))
+    return render_template("login.html", form=form)
 
 
-
-@app.route('/dashboard', methods=['GET', 'POST'])
+@app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route("/logout", methods=["GET", "POST"])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
 
-
-@ app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
 
@@ -111,49 +125,51 @@ def register():
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
 
-
-@app.route('/about')
+@app.route("/about")
 @login_required
 def about_page():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/team')
+
+@app.route("/team")
 @login_required
 def team_page():
-    return render_template('team.html')
+    return render_template("team.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 @login_required
 def contact_page():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
-@app.route('/services',methods=["GET","POST"])
+
+@app.route("/services", methods=["GET", "POST"])
 @login_required
 def predict():
-    if request.method=="GET":
-        return render_template('services.html')
-    else:  
-        int_features=[[float(x) for x in request.form.values()]]
+    if request.method == "GET":
+        return render_template("services.html")
+    else:
+        int_features = [[float(x) for x in request.form.values()]]
         print(int_features)
-        
-        final=np.array(int_features)
-        print(final.shape)
-        
-        pred=model.predict_proba(final)
-        print(pred[0])
-        
-        if(pred[0][1]<0.5):
-            
-            return render_template('services.html',pred_text="Fraud")
-    
-        else:
-            return render_template('services.html',pred_text="not Fraud")
-            
 
-if __name__=="__main__":
-    app.run(debug=True)
+        final = np.array(int_features)
+        print(final.shape)
+
+        pred = model.predict_proba(final)
+        print(pred[0])
+
+        if pred[0][1] < 0.5:
+
+            return render_template("services.html", pred_text="Fraud")
+
+        else:
+            return render_template("services.html", pred_text="not Fraud")
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
